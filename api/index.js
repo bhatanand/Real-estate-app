8a055931a185a32bddf6f4e5f9d93a92213029fe
Anand Bhat <bhatanand360@gmail.com>
2024-07-07 10:26:27 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+
+const mongo_url="mongodb+srv://bhatanand360:qw12er34ty56@mern-real-estate.esorw8i.mongodb.net/?retryWrites=true&w=majority&appName=mern-real-estate";
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1-Mt9jbbvciujS290L0Cn0U9KcEx1yo0",
+  authDomain: "mern-real-estate-df0a5.firebaseapp.com",
+  projectId: "mern-real-estate-df0a5",
+  storageBucket: "mern-real-estate-df0a5.appspot.com",
+  messagingSenderId: "377327910199",
+  appId: "1:377327910199:web:6851c17e018fead1faeeb2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
